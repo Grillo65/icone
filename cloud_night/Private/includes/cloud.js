c020bf4da2387564c8124d4b14002ca2d536f8a6
Grillo65 <131480376+Grillo65@users.noreply.github.com>
2024-01-14 16:16:22 +0100

    Add files via upload

---
+++ cloud_night/Private/includes/cloud.js
@@ -0,0 +1,36 @@
+const NUMBER_OF_CLOUDS = 8; // clouds to show on screen
+const NUMBER_OF_IMAGES2 = 16; // images + 1
+function init2() { 
+    var container2 = document.getElementById("CloudContainer");
+    for (var i = 0; i < NUMBER_OF_CLOUDS; i++) {
+	container2.appendChild(createACloud())}
+}
+function randomInteger(low, high) {
+    return low + Math.floor(Math.random() * (high - low));
+}
+function randomFloat(low, high) {
+    return low + Math.random() * (high - low);
+}
+function pixelValue(value) {
+    return value + "%";
+}
+function durationValue(value) {
+    return value + "s";
+}
+function createACloud() {
+    var cloudDiv = document.createElement("div");
+    var image = document.createElement("img");
+    image.src = "cloud_night/Private/images/cloud" + randomInteger(1, NUMBER_OF_IMAGES2) + ".png";
+    cloudDiv.style.top = pixelValue(randomInteger(-2, 8));
+    // cloudDiv.style.left = pixelValue(randomInteger(-400, -100));
+    var spinAnimationName = (Math.random() < 0.5) ? "clockwiseSpin" : "counterclockwiseSpinAndFlip";
+    cloudDiv.style.webkitAnimationName = "fade, float";
+    image.style.webkitAnimationName = spinAnimationName;
+    var fadeAndfloatDuration = durationValue(randomFloat(100, 250));
+    var spinDuration = durationValue(randomFloat(40, 80));
+    cloudDiv.style.webkitAnimationDuration = fadeAndfloatDuration + ", " + fadeAndfloatDuration;
+    image.style.webkitAnimationDuration = spinDuration;
+    cloudDiv.appendChild(image);
+    return cloudDiv;
+}
+window.addEventListener("load", init2, false);
